f88ec8e4900fd210466cfacc39409f5812f81cc5
Celeste McCall <celestemccall93@gmail.com>
2023-02-15 21:02:02 -0600

    Initial Commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
 
+        public void MyTest(string word, bool expected)
+        {
+            //arrange
+            var palindrometester = new WordSmith();
+
+            //act
+
+            var actual = palindrometester.IsAPalindrome(word);
+
+            //assert
+
+            Assert.Equal(expected, actual);
         }
+
+        
     }
 }
